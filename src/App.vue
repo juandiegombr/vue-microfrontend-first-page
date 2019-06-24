60e23e28e7402d7cbc3fce1b9378679a9853608f
juandiegombr <juandiego.mbr@gmail.com>
2019-06-24 09:26:25 +0200

    More routing examples & Events

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,34 @@
 <template>
-  <div id="First-container" class="page">
+  <div ref="firstContainer" id="First-container" class="page">
     <h1>First Microfrontend</h1>
     <p>This is a MicroFrontend comming from a remote project</p>
-    <button @click="goHome">Go home from MicroFrontend</button>
+    <p>Routing from Microfrontend</p>
+    <button @click="goHome">Go to Home</button>
+    <button @click="goToImage(1051)">Image 1</button>
+    <button @click="goToImage(1043)">Image 2</button>
+    <button @click="goToImage(1036)">Image 3</button>
+    <p>Communication between MicroFrontends</p>
+    <button @click="changeUser">Change User</button>
   </div>
 </template>
 
 <script>
+import faker from 'faker'
 export default {
   name: 'First',
   methods: {
     goHome () {
-      console.log(this)
       this.$router.push('/')
+    },
+    goToImage (id) {
+      this.$router.push(`/second/${id}`)
+    },
+    changeUser () {
+      const event = new CustomEvent('changeUser', {
+        bubbles: true,
+        detail: { name: faker.name.findName(), image: `https://picsum.photos/id/${(Math.random() * 1000).toFixed()}/24/24` }
+      })
+      this.$refs.firstContainer.dispatchEvent(event)
     }
   },
   created () {
